0f52404b76c56f923a9883abfd80ef086486229e
Vito <zhangwei.noair@gmail.com>
2014-09-26 10:59:15 +0800

    调整ItemDetail 页面 contentView没有全屏的问题

--- iWardrobe/ItemDetailViewController.m
+++ iWardrobe/ItemDetailViewController.m
@@ -12,6 +12,7 @@
 @interface ItemDetailViewController ()
 
 @property (weak, nonatomic) IBOutlet UIScrollView *scrollView;
+@property (weak, nonatomic) IBOutlet UIView *contentView;
 @property (weak, nonatomic) IBOutlet UIImageView *imageView;
 @property (weak, nonatomic) IBOutlet UILabel *itemNameLabel;
 @property (weak, nonatomic) IBOutlet UILabel *brandLabel;
@@ -26,15 +27,42 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     self.automaticallyAdjustsScrollViewInsets = NO;
+    
+    [self setupUI];
+    [self setupData];
+}
+
+- (void)setupUI
+{
+    NSLayoutConstraint *leftConstraint = [NSLayoutConstraint constraintWithItem:self.contentView
+                                                                      attribute:NSLayoutAttributeLeading
+                                                                      relatedBy:0
+                                                                         toItem:self.view
+                                                                      attribute:NSLayoutAttributeLeft
+                                                                     multiplier:1.0
+                                                                       constant:0];
+    [self.view addConstraint:leftConstraint];
+    
+    NSLayoutConstraint *rightConstraint = [NSLayoutConstraint constraintWithItem:self.contentView
+                                                                       attribute:NSLayoutAttributeTrailing
+                                                                       relatedBy:0
+                                                                          toItem:self.view
+                                                                       attribute:NSLayoutAttributeRight
+                                                                      multiplier:1.0
+                                                                        constant:0];
+    [self.view addConstraint:rightConstraint];
+}
+
+- (void)setupData
+{
     self.imageView.image = self.item.image;
     self.itemNameLabel.text = self.item.name;
     self.brandLabel.text = self.item.brand;
     
     NSNumberFormatter *numberFormatter = [[NSNumberFormatter alloc] init];
     [numberFormatter setNumberStyle:NSNumberFormatterCurrencyStyle];
+    [numberFormatter setLocale:self.item.currencyLocale];
     self.priceLabel.text = [numberFormatter stringFromNumber:self.item.price];
-    
-    
 }
 
 #pragma mark - UICollectionViewDataSource
