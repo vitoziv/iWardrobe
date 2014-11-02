f8d4ab68e97624626fcb1a394926a822f909b1e0
Vito <zhangwei.noair@gmail.com>
2014-11-02 16:10:35 +0800

    修改 item detail 页面，让实现简单一点，以后再细调

--- iWardrobe/HomeViewController.m
+++ iWardrobe/HomeViewController.m
@@ -24,4 +24,11 @@
     NSLog(@"back to home");
 }
 
+#pragma mark - UITableViewDelegate
+
+- (void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    [tableView deselectRowAtIndexPath:indexPath animated:YES];
+}
+
 @end
