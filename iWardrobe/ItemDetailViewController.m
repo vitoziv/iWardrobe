7b2532758d58f6831d52ab04a2a299b06de8fd76
Vito <zhangwei.noair@gmail.com>
2014-12-11 08:37:43 +0800

    Item 详情页面显示 tag 信息

--- iWardrobe/ItemDetailViewController.m
+++ iWardrobe/ItemDetailViewController.m
@@ -9,12 +9,18 @@
 #import "ItemDetailViewController.h"
 #import "Item.h"
 #import "Info.h"
+#import "Tag.h"
 #import "IWStringInfoCell.h"
+
+static NSString *const CellIdentifierKey = @"CellIdentifier";
+static NSString *const SectionDataKey = @"SectionData";
 
 @interface ItemDetailViewController () <UITableViewDelegate, UITableViewDataSource, UIScrollViewDelegate>
 
 @property (weak, nonatomic) IBOutlet UITableView *tableView;
 @property (weak, nonatomic) IBOutlet UIImageView *imageView;
+
+@property (strong, nonatomic) NSArray *data;
 
 @end
 
@@ -25,6 +31,7 @@
     self.automaticallyAdjustsScrollViewInsets = NO;
 
     [self setupView];
+    [self setupData];
 }
 
 - (void)setupView
@@ -32,20 +39,38 @@
     self.imageView.image = self.item.image;
 }
 
+- (void)setupData
+{
+    NSMutableString *tagString = [NSMutableString string];
+    [self.item.tags enumerateObjectsUsingBlock:^(Tag *tag, NSUInteger idx, BOOL *stop) {
+        [tagString appendString:[NSString stringWithFormat:@"%@  ", tag.name]];
+    }];
+    self.data = @[@{CellIdentifierKey: @"TagCell", SectionDataKey: @[[tagString copy]]}, @{CellIdentifierKey: @"StringInfoCell", SectionDataKey: self.item.infos}];
+}
+
 #pragma mark - UITableViewDataSource
+
+- (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView
+{
+    return self.data.count;
+}
 
 - (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section
 {
-    return self.item.infos.count;
+    return [self.data[section][SectionDataKey] count];
 }
 
 - (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath
 {
-    static NSString *StringInfoCellIdentifier = @"StringInfoCell";
-    IWStringInfoCell *cell = [tableView dequeueReusableCellWithIdentifier:StringInfoCellIdentifier forIndexPath:indexPath];
-
-    Info *info = self.item.infos[indexPath.row];
-    [cell configureWithInfo:info];
+    NSString *CellIdentifier = self.data[indexPath.section][CellIdentifierKey];
+    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:CellIdentifier forIndexPath:indexPath];
+    if ([CellIdentifier isEqualToString:@"TagCell"]) {
+        NSString *tagString = [self.data[indexPath.section][SectionDataKey] lastObject];
+        cell.textLabel.text = tagString;
+    } else if ([CellIdentifier isEqualToString:@"StringInfoCell"]) {
+        Info *info = self.data[indexPath.section][SectionDataKey][indexPath.row];
+        [(IWStringInfoCell *)cell configureWithInfo:info];
+    }
     
     return cell;
 }
